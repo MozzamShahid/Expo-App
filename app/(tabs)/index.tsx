a5f5d061d165d96eaea8bb08bd326d1589cd678f
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2025-12-23 18:59:22 +0500

    Chapter 5 Create Modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,100 @@
 import Buttons from "@/components/Buttons";
+import CircleButton from "@/components/CircleButton";
+import EmojiList from "@/components/EmojiList";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiSticker from "@/components/EmojiSticker";
+import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ImageSourcePropType, StyleSheet, View } from "react-native";
+
 
 const PlaceholderImage = require('../../assets/images/background-image.png')
 
 export default function Index() {
-  const [selectedImage, setselectedImage] = useState<string | undefined>(undefined)
+  const [selectedImage, setselectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setshowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setisModalVisible] = useState<boolean>(false);
+  const [PickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
 
-  const pickImageAsync = async() => {
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-    allowsEditing: true,
-    quality: 1,
+      allowsEditing: true,
+      quality: 1,
     })
 
-    if(!result.canceled){
-      setselectedImage(result.assets[0].uri)
+    if (!result.canceled) {
+      setselectedImage(result.assets[0].uri);
+      setshowAppOptions(true);
     } else {
       alert('Pick an image')
     }
   }
 
+  const onReset = () =>{
+    setshowAppOptions(false);
+  }
+
+  const onAddSticker = () => {
+    setisModalVisible(true);
+  }
+
+  const onModalClose = () => {
+    setisModalVisible(false);
+  }
+
+  const onSaveImageAsync = () => {
+    
+  }
+
   return (
     <View style={styles.container}>
-     <View style={styles.imagecontainer}>
-      <ImageViewer ImageSource={selectedImage || PlaceholderImage} />
-     </View>
-     <View style={styles.containersec}>
-      <Buttons onPress={pickImageAsync} theme='primary' label="Choose a Photo"/>
-      <Buttons label="Use this Photo"/>
-     </View>
-      
+      <View style={styles.imagecontainer}>
+        <ImageViewer ImageSource={selectedImage || PlaceholderImage} />
+        {PickedEmoji && <EmojiSticker imageSize={40} stickerSource={PickedEmoji}/>}
+      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+          <IconButton icon="refresh" label="Reset" onPress={onReset} />
+          <CircleButton onPress={onAddSticker}/>
+          <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+
+      ) : (
+        <View style={styles.containersec}>
+          <Buttons onPress={pickImageAsync} theme='primary' label="Choose a Photo" />
+          <Buttons label="Use this Photo" onPress={() => {setshowAppOptions(true), setisModalVisible(true)} } />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-   backgroundColor: '#25292e',
+  container: {
+    backgroundColor: '#25292e',
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  imagecontainer:{
+  imagecontainer: {
     flex: 1,
   },
-  containersec:{
-    flex: 1/3,
+  containersec: {
+    flex: 1 / 3,
   },
- 
+   optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+   optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
 })
---
+++ components/IconButton.tsx
@@ -0,0 +1,28 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Pressable, StyleSheet, Text } from 'react-native';
+
+type Props = {
+    icon: keyof typeof MaterialIcons.glyphMap;
+    label: string;
+    onPress: () => void;
+}
+
+export default function IconButton({onPress, icon, label}: Props){
+return(
+        <Pressable style={styles.iconButton}>
+           <MaterialIcons name={icon} size={24} color="#fff" />
+            <Text style={styles.iconButtonLabel}>{label}</Text>
+        </Pressable>
+)
+}
+
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconButtonLabel: {
+    color: '#fff',
+    marginTop: 12,
+  },
+});
